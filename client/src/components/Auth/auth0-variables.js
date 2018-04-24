0c91304faed6d42a554c16573751655a0f91b7d4
anishbnair <nbanish@gmail.com>
2018-04-24 10:45:05 -0400

    updated domain clientid and callbackurls

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -1,5 +1,9 @@
 export const AUTH_CONFIG = {
-    domain: 'project-3.auth0.com',
-    clientId: 'a6R3KKabd2LumdWZAafpqffgbUmeQrce',
-    callbackUrl: 'http://localhost:3000/callback'
+    // domain: 'project-3.auth0.com',
+    // clientId: 'a6R3KKabd2LumdWZAafpqffgbUmeQrce',
+    // callbackUrl: 'http://localhost:3000/callback'
+
+    domain: 'app94688708.auth0.com',
+    clientId: 'lRJjwPhtHxwATPLCUVZpwKbl1Sj20rd6',
+    callbackUrl: 'https://mentor-match-learn2code/callback'
 }
