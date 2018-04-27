fd75e7fbe24c05d90fa7a72f01f17647c66e709b
anishbnair <nbanish@gmail.com>
2018-04-26 20:38:33 -0400

    commented react component

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,15 +1,16 @@
 // imports React and its components and the logo image (logo.png)
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+import React from 'react';
 import logo from "../../images/Logo.jpg";
 import './Logo.css';
 
 // creates a Logo class which extends the React component
 const Logo = () => {
-    return (
-      <div id="logo" className="Logo">
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-    );
-  }
+  return (
+    <div id="logo" className="Logo">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
 
 export default Logo;
