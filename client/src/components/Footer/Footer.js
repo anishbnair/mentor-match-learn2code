1c3040fb8a7e0216695db9ba4be90f0b9cf7cb42
anishbnair <nbanish@gmail.com>
2018-04-26 20:37:34 -0400

    commented react component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,15 @@
 // imports React and its components and the logo image (logo.png)
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+import React from 'react';
 import './Footer.css';
 
 // creates a Footer class which extends the React component
 const Footer = () => {
-    return (
-      <div id="footer" className="Footer">
-    	<p>Copyright &copy; 2018 Code Junkies</p>
-      </div>
-    );
-  }
+  return (
+    <div id="footer" className="Footer">
+      <p>Copyright &copy; 2018 Code Junkies</p>
+    </div>
+  );
+}
 
 export default Footer;
