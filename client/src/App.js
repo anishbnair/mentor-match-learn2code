d72b7b0cfc26475e23ccd1eee30b701281369aea
castillo.claudia.m.s@gmail.com <castillo.claudia.m.s@gmail.com>
2018-04-26 20:04:52 -0400

    new files

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Navbar, Button } from 'react-bootstrap';
 import './App.css';
 
+import Wrapper from "./components/Wrapper";
+import Logo    from "./components/Logo";
+import Footer  from "./components/Footer";
+
+
 class App extends Component {
   goTo(route) {
     this.props.history.replace(`/${route}`)
@@ -19,60 +24,75 @@ class App extends Component {
     const { isAuthenticated } = this.props.auth;
 
     return (
-      <div>
-        <Navbar fluid>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="/">Mentor Match - Learn to Code</a>
-            </Navbar.Brand>
-            <Button
-              bsStyle="primary"
-              className="btn-margin"
-              onClick={this.goTo.bind(this, 'home')}
-            >
-              Home
-            </Button>
-            {
-              !isAuthenticated() && (
-                <Button
-                  bsStyle="primary"
-                  className="btn-margin"
-                  onClick={this.login.bind(this)}
-                >
-                  Log In
-                  </Button>
-              )
-            }
-            {
-              isAuthenticated() && (
-                <Button
-                  bsStyle="primary"
-                  className="btn-margin"
-                  onClick={this.goTo.bind(this, 'profile')}
-                >
-                  Profile
-                  </Button>
-              )
-            }
-            {
-              isAuthenticated() && (
-                <Button
-                  bsStyle="primary"
-                  className="btn-margin"
-                  onClick={this.logout.bind(this)}
-                >
-                  Log Out
-                  </Button>
-              )
-            }
-          </Navbar.Header>
-        </Navbar>
-        <div className="container">
-          {this.props.children}
+      <Wrapper>
+
+        <Logo />
+
+        <div>  
+          <Navbar fluid>
+            <Navbar.Header>
+              
+              <Button
+                data-primary={true}
+                className="Button"
+                onClick={this.goTo.bind(this, 'home')}
+              >
+                Home
+              </Button>
+              {
+                !isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="Button"
+                    data-primary={true}
+                    className="Button"
+                    // className="Button"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                    </Button>
+                )
+              }
+              {
+                isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    data-primary={true}
+                    className="Button"
+                    onClick={this.goTo.bind(this, 'profile')}
+                  >
+                    Profile
+                    </Button>
+                )
+              }
+              {
+                isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    data-primary={true}
+                    className="Button"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                    </Button>
+                )
+              }
+            </Navbar.Header>
+          </Navbar>
+          <div className="container">
+            {this.props.children}
+          </div>
         </div>
-      </div>
+
+        <Footer />
+
+      </Wrapper>
+
     );
   }
 }
 
 export default App;
+
---
+++ client/src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+// imports React and its components and the logo image (logo.png)
+import React, {Component} from 'react';
+import logo from "../../images/Logo.jpg";
+import './Logo.css';
+
+// creates a Logo class which extends the React component
+const Logo = () => {
+    return (
+      <div id="logo" className="Logo">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
+  }
+
+export default Logo;
