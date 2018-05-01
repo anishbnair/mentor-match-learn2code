afbdef7c70fd9076e5fae3a2f24804fbeaa5473d
kalaunlimited <KLasode@gmail.com>
2018-04-30 22:47:06 -0400

    checkpoint save: currently working on resources,users,userscontroller,and resource

--- controllers/resourcesController.js
+++ controllers/resourcesController.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the ResourcesController
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    db.Resource
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Resource
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.Resource
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.Resource
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Resource
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
