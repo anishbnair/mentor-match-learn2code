2fd932b357ac65d2adb39215c156ac4d652ab3e0
anishbnair <nbanish@gmail.com>
2018-05-01 21:22:09 -0400

    updated connect.js file for connect page

--- client/src/pages/Connect.js
+++ client/src/pages/Connect.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
+import Mentor from "../components/Mentor/Mentor.js";
+import sampleMentors from "../components/Mentor/seeds-mentor.js";
 
 class Connect extends Component {
+  state = {
+    mentors: {}
+  };
+
+  componentDidMount() {
+    this.loadMentors();
+  }
+
+  loadMentors = () => {
+    this.setState({ mentors: sampleMentors });
+  };
+
   render() {
-    return <p>Mentor details from database will be fetched here!!!</p>
+    return (
+      <div className="mentor-connect">
+        <div className="mentors">
+          <ul className="mentor">
+            {Object.keys(this.state.mentors).map(key => (
+              <Mentor key={key} index={key} details={this.state.mentors[key]} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
