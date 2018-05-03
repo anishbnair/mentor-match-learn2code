598273e6bcd85a7337526df696d21a9a60613904
kalaunlimited <KLasode@gmail.com>
2018-05-03 00:26:23 -0400

    server.js file setup

--- client/src/pages/Connect.js
+++ client/src/pages/Connect.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 // api.get user
-=======
-import Mentor from "../components/Mentor/Mentor.js";
-import sampleMentors from "../components/Mentor/seeds-mentor.js";
-
->>>>>>> 374242e3b7b4b9bf9a93b082bce66be96b1f97aa
 class Connect extends Component {
   state = {
     mentors: {}
@@ -20,22 +14,8 @@ class Connect extends Component {
   };
 
   render() {
-<<<<<<< HEAD
     return <p>Mentor details from database will be fetched here!!!</p>
   
-=======
-    return (
-      <div className="mentor-connect">
-        <div className="mentors">
-          <ul className="mentor">
-            {Object.keys(this.state.mentors).map(key => (
-              <Mentor key={key} index={key} details={this.state.mentors[key]} />
-            ))}
-          </ul>
-        </div>
-      </div>
-    );
->>>>>>> 374242e3b7b4b9bf9a93b082bce66be96b1f97aa
   }
 }
 
