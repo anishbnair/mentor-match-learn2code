e8a9d1a5e8841dc52e7f4609d629cbbd9f883226
anishbnair <nbanish@gmail.com>
2018-05-03 14:46:16 -0400

    updated files

---
+++ client/src/components/Mentor/seeds-mentor.js
@@ -0,0 +1,23 @@
+// This is just some sample data so you don't have to think of your own!
+const mentors = {
+  mentor1: {
+    name: "John Doe",
+    email: "john.doe@gmail.com",
+    github: "https://github.com/johndoe",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/MarioSMBW.png/220px-MarioSMBW.png",
+    desc: "I am a front-end web developer who loves to code.",
+    skill: "HTML, CSS, JavaScript"
+  },
+
+  mentor2: {
+    name: "Jane Doe",
+    email: "jane.doe@gmail.com",
+    github: "https://github.com/janedoe",
+    image: "https://upload.wikimedia.org/wikipedia/en/f/f1/LuigiNSMBW.png",
+    desc: "I am a back-end web developer.",
+    skill: "Node.js, Express, MongoDB"
+  }
+};
+
+export default mentors;
---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+import ReactDOM from 'react-dom';
+import './index.css';
+// import './bootstrap/dist/css/bootstrap.css';
+import { makeMainRoutes } from './routes/routes';
+
+const routes = makeMainRoutes();
+
+ReactDOM.render(
+    routes,
+    document.getElementById('root')
+);
