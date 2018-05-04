5b593cf1a99b520e524f2693a84f382146f5dbf8
Matt <mvt3641@gmail.com>
2018-05-04 13:43:35 -0400

    console log autho token

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -45,6 +45,7 @@ export default class Auth {
         let expiresAt = JSON.stringify(
             authResult.expiresIn * 1000 + new Date().getTime()
         );
+        console.log(authResult);
         localStorage.setItem('access_token', authResult.accessToken);
         localStorage.setItem('id_token', authResult.idToken);
         localStorage.setItem('expires_at', expiresAt);
@@ -81,7 +82,7 @@ export default class Auth {
     }
 
     isAuthenticated() {
-        // Check whether the current time is past the 
+        // Check whether the current time is past the
         // access token's expiry time
         let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
         return new Date().getTime() < expiresAt;
