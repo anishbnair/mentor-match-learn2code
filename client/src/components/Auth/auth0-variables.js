265c70cac410e088f6fbbc6667f760de49dc2de0
anishbnair <nbanish@gmail.com>
2018-05-05 13:22:44 -0400

    reverted changes

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -1,13 +1,17 @@
 export const AUTH_CONFIG = {
-  domain: "mentor-match.auth0.com",
-  clientId: "srfL4vr5SB0gSsS8yne5OQxJjhWFKMKc",
-  callbackUrl:
-    process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
-
-  // domain: "project-3.auth0.com",
-  // clientId: "a6R3KKabd2LumdWZAafpqffgbUmeQrce",
+  // domain: "mentor-match.auth0.com",
+  // clientId: "srfL4vr5SB0gSsS8yne5OQxJjhWFKMKc",
   // callbackUrl:
   //   process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+
+  // domain: "mentor-match.auth0.com",
+  // clientId: "srfL4vr5SB0gSsS8yne5OQxJjhWFKMKc",
+  // callbackUrl:
+  //   process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+
+  domain: "project-3.auth0.com",
+  clientId: "a6R3KKabd2LumdWZAafpqffgbUmeQrce",
+  callbackUrl: "http://localhost:3000/callback"
 
   // domain: 'app94688708.auth0.com',
   // clientId: 'lRJjwPhtHxwATPLCUVZpwKbl1Sj20rd6',
