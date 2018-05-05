1b3f94dc4d11322bb5080ff613d0de160f162d4d
Matt <mvt3641@gmail.com>
2018-05-05 14:14:00 -0400

    jacked up commit

--- routes/api/dashapi.js
+++ routes/api/dashapi.js
@@ -1,12 +1,19 @@
 var router = require("express").Router();
-// var resourcesController= require('../controllers/resourcesController');
-//  var userController = require('../controllers/userController');
+// var resourcesController= require('../../controllers/resourcesController');
+// var usersController = require('../../controllers/usersController');
+var mongoose=require("mongoose");
 
 
 router.route("/")
 .post(function(req,res){
-  console.log("These are the results!  " +res);
-  res.json("results");
+  console.log("These are the results!  " +JSON.parse(res));
+  var uri = "mongodb://localhost:27017/mentor-match"
+mongoose.connect(uri, function(error) {
+  if (error) throw error;
+  console.log(" database connected");
+
+})
+res.json(res)
 });
 
 // router.route("/api/dashboard")
