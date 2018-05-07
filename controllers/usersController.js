4f3d71f88ccdf25e5d802a3630dc937aeb6f6d81
kalaunlimited <KLasode@gmail.com>
2018-05-07 00:27:59 -0400

    checkpoint save: users are now  saved in the database next step is to check if user exist

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,6 +9,13 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  findOne: function(req, res) {
+    db.User
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
   findById: function(req, res) {
     db.User
       .findById(req.params.id)
