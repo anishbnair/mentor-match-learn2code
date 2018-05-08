a75a2557f5c1a934a1aa679de361788c745fd901
kalaunlimited <KLasode@gmail.com>
2018-05-08 08:59:22 -0400

    deted uncommented code

--- routes/api/dashapi.js
+++ routes/api/dashapi.js
@@ -8,51 +8,6 @@ var Users = require("../../models/Users")
 
 
 router.route("/").post(usersController.findOne)
-// router.route("/").get(usersController.findById)
-// router.route("/").post(usersController.create)
-// .post(function(req,res){
-//   console.log(req.body.idTokenPayload.nickname);
-//   res.json("results please");
-//   uri = "mongodb://localhost:27017/mentor-match"
-//   mongoose.connect(uri, (error)=>{
 
-//     var Newuser = new Users({ name: req.body.idTokenPayload.name,
-//       email:req.body.idTokenPayload.nickname+"@gmail.com",
-//       picture:req.body.idTokenPayload.picture});
-//       Newuser.save((err) =>{
-//         if(err) throw err;
-//         console.log(Newuser);
-//       });
-//     console.log("database connected");
-//   })
-
-// });
-
-
-// , function(req,res){
-  // uri = "mongodb://localhost/mentor-match"
-  // // console.log(req.body);
-  // console.log(req.body.idTokenPayload.name);
-  // console.log(req.body. idTokenPayload.nickname+"@gmail.com");
-  // console.log(req.body.idTokenPayload.picture);
-  //
-  // mongoose.connect(uri, (error)=>{
-  //   var User = new Users({ name: req.body.idTokenPayload})
-  //   console.log("database connected")
-  // })
-  // // console.log(res);
-  // res.json("results");
-
-// router.route("/api/dashboard")
-//   .get(userController.findById)
-//   .post(userController.create)
-//   .post(userController.update)
-//
-//
-// router
-//   .route("/api/resources")
-//   .get(resourcesController.findAll)
-//   .put(resourcesController.update)
-//   .delete(resourcesController.delete)
 
 module.exports = router;
