039931067653b48056d561ef907f6655d7e664a3
Anish B Nair <nbanish@gmail.com>
2018-05-08 11:57:35 -0400

    Update auth0-variables.js

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -22,7 +22,7 @@ export const AUTH_CONFIG = {
   //   // callbackUrl: "http://localhost:3000/callback"
   //   callbackUrl: process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
 
-  domain: "mentor-match.auth0.com",
-  clientId: "07VYWunyh6eFyo25KrcLuD8Cynu9PvY6",
-  callbackUrl: "https://mentor-match-learn2code.herokuapp.com/callback"
+  domain: 'mentor-match.auth0.com',
+  clientId: '07VYWunyh6eFyo25KrcLuD8Cynu9PvY6',
+  callbackUrl: 'https://mentor-match-learn2code.herokuapp.com/callback'
 };
