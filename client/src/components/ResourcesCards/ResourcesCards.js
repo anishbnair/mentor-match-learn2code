b043f83429e7f38f322d2999ff8feafd67b34b6b
daniela jaros <danielajaros@gmail.com>
2018-05-08 21:24:39 -0400

    created resources logos page layout

---
+++ client/src/components/ResourcesCards/ResourcesCards.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Panel, Button} from 'react-bootstrap';
+import "./ResourcesCards.css";
+
+
+const ResourceCard= props=> (
+
+        <div>
+            <Panel className="tech-cards">
+                <Panel.Body><img alt={props.name} src={props.image} /></Panel.Body>
+                <Panel.Footer><Button bsStyle="primary">Resources</Button></Panel.Footer>
+            </Panel>    
+        </div>
+    );
+
+
+export default ResourceCard; 
