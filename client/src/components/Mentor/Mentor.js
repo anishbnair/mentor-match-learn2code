e718af8f2d4ed8d7c0069e2535f40f43be1c7af3
GeneCannella <gene.cannella@gmail.com>
2018-05-09 17:24:54 -0400

    adding React button component to Mentor component

--- client/src/components/Mentor/Mentor.js
+++ client/src/components/Mentor/Mentor.js
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
+import { Button } from 'react-bootstrap';
 import "./Mentor.css";
 
 class Mentor extends Component {
+
+  handleButton () {
+    alert("Hello World!");
+  }
+
+
   render() {
     //   const image = this.props.details.image;
     const { image, name, email, github, skill, desc } = this.props.details;
 
     return (
-      <li className="mentor-list">
-        <img src={image} alt={name} />
-        <h3 className="mentor-name">Name: {name}</h3>
-        <p>email: {email}</p>
-        <p>GitHub: {github}</p>
-        <p>Skill: {skill}</p>
-        <p>About me: {desc}</p>
-      </li>
+      <div className="container">
+        <div className="row">
+          
+          <div className="col-lg-12">
+            <li className="mentor-list">
+              <img src={image} alt={name} />
+              <h3 className="mentor-name">Name: {name}</h3>
+              <p>email: {email}</p>
+              <p>GitHub: {github}</p>
+              <p>Skill: {skill}</p>
+              <p>About me: {desc}</p>
+            </li>
+            <Button type="button" onClick={this.handleButton}>Connect</Button>
+          </div>
+
+        </div>
+      </div>
     );
   }
 }
