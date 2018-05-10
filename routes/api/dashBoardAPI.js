f581f861f1dcc9af82a292b6e004a723e6863761
anishbnair <nbanish@gmail.com>
2018-05-10 12:16:50 -0400

    merging cludias dashBoardAPI.js updates to master

---
+++ routes/api/dashBoardAPI.js
@@ -0,0 +1,10 @@
+var router = require("express").Router();
+var resourcesController = require("../../controllers/resourcesController");
+var usersController = require("../../controllers/usersController");
+var mongoose = require("mongoose");
+var Users = require("../../models/Users");
+
+//Do we actually need to require resourcesController, mongoose, or Users?
+router.route("/").get(usersController.findOne);
+
+module.exports = router;
