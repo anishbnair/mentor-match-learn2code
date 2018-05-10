83232e9bf895185f3a48a8e2e62864b41c52a5d7
anishbnair <nbanish@gmail.com>
2018-05-10 17:47:27 -0400

    merging claudias index.js updates to master

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,12 @@ const userRoute = require("./dashapi");
 const signInRoute = require("./signapi");
 const dashBoardRoute = require("./dashBoardAPI");
 const connectRoute = require("./connectAPI");
+const resourcesRoute = require("./resourcesAPI");
 
 router.use("/dashboard", dashBoardRoute);
-
 router.use("/connect", connectRoute);
-
 router.use("/home", userRoute);
 router.use("/home", signInRoute);
+router.use("/resources", resourcesRoute);
+
 module.exports = router;
