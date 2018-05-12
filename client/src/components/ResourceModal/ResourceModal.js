7adbf2bb63ea975e2f8a6806f539c4e047957d38
anishbnair <nbanish@gmail.com>
2018-05-11 22:43:05 -0400

    merging claudias ResourceModal.js changes to master

--- client/src/components/ResourceModal/ResourceModal.js
+++ client/src/components/ResourceModal/ResourceModal.js
@@ -1,61 +1,92 @@
-import React from 'react'; 
-import {Button, Modal,OverlayTrigger,Popover,Tooltip} from 'react-bootstrap';
-import ResourceList from "../ResourceList/ResourceList"
+import React from "react";
+import {
+  Button,
+  Modal,
+  OverlayTrigger,
+  Popover,
+  Tooltip
+} from "react-bootstrap";
+import ResourceList from "../ResourceList/ResourceList";
 import "./ResourceModal.css";
-
+import API from "../../utils/API";
 
 export default class ResourceModal extends React.Component {
-    
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-        this.handleShow = this.handleShow.bind(this);
-        this.handleClose = this.handleClose.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
 
-        this.state = {
-            show: false
-        };
-    }
+    this.state = {
+      show: false,
+      resources: []
+    };
+  }
 
-    handleClose() {
-        this.setState({
-            show: false
-        });
-    }
+  loadResources = () => {
+    API.showResourceList()
+      .then(res => {
+        const resources = Object.entries(res.data[0])
+          .filter(([name, links]) => name === this.props.rscdetails.name)
+          .reduce(
+            (a, [name, { type: b }]) => (Array.isArray(b) ? [...a, ...b] : a),
+            []
+          );
+        console.log("success with: ", this.props.rscdetails.name);
+        console.log(res.data);
+        console.log(resources);
+        this.setState({ resources });
+      })
+      .catch(err => console.log("fail"));
+  };
 
-    handleShow() {
-        this.setState({
-            show: true
-        });
-    }
+  handleClose() {
+    this.setState({
+      show: false
+    });
+  }
 
-    render() {
-        const popover = ( 
-            <Popover id="modal-popover"   title="popover">
-            very popover.such engagement 
-            </Popover>
-        );
-        const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
+  handleShow() {
+    this.loadResources();
+    console.log("this props name: ", this.props.rscdetails.name);
+    this.setState({
+      show: true
+    });
+  }
 
-        return ( 
-            <div>
+  render() {
+    const popover = (
+      <Popover id="modal-popover" title="popover">
+        very popover.such engagement
+      </Popover>
+    );
+    const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
 
-            {/* <Button bsStyle = "primary" bsSize="small" onClick={this.handleShow}>Show Resources</Button> */}
-            <img alt={this.props.image.name} src={this.props.image.image} onClick={this.handleShow} />
+    return (
+      <div>
+        {/* <Button bsStyle = "primary" bsSize="small" onClick={this.handleShow}>Show Resources</Button> */}
+        <img
+          alt={this.props.rscdetails.name}
+          src={this.props.rscdetails.image}
+          onClick={this.handleShow}
+        />
 
-            <Modal className = "modal"show = {this.state.show} onHide = {this.handleClose} >
-            <Modal.Header closeButton>
-            <Modal.Title>Resources</Modal.Title> 
-            </Modal.Header> 
-            <Modal.Body>
-            
-                <ResourceList />
-            </Modal.Body> 
-            <Modal.Footer >
-            <Button onClick={this.handleClose}> Close</Button> 
-            </Modal.Footer> 
-            </Modal> 
-            </div>
-        );
-    }
+        <Modal
+          className="modal"
+          show={this.state.show}
+          onHide={this.handleClose}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Resources</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <ResourceList list={this.state.resources} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleClose}> Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 }
