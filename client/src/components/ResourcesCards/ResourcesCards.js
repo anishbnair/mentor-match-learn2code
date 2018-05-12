397e9b7e5326946fc1d7d24f29eab8f0d70de7c7
anishbnair <nbanish@gmail.com>
2018-05-11 22:44:16 -0400

    merging claudias ResourcesCards.js updates to master

--- client/src/components/ResourcesCards/ResourcesCards.js
+++ client/src/components/ResourcesCards/ResourcesCards.js
@@ -2,19 +2,15 @@ import React from "react";
 import "./ResourcesCards.css";
 import ResourceModal from "../ResourceModal/ResourceModal";
 
-
 const ResourceCard = props => (
-
-    <div className="resources">
-        <ResourceModal
-            image={{
-                image: props.image,
-                name: props.name
-            }}
-        />
-    </div>
+  <div className="resources">
+    <ResourceModal
+      rscdetails={{
+        image: props.image,
+        name: props.name
+      }}
+    />
+  </div>
 );
 
-
 export default ResourceCard;
-
