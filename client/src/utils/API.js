12024f8d47915d97c32bd67f1cb42a586ce4b79b
anishbnair <nbanish@gmail.com>
2018-05-11 22:49:09 -0400

    merging claudias API.js updates to master

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,65 +1,59 @@
 import axios from "axios";
 
-
 export default {
-
-    //user is first checked in database and will be created if not found in database on initial sign in
-  createUser:function(userinfo){
-    return axios.post('api/home',userinfo)
+  //user is first checked in database and will be created if not found in database on initial sign in
+  createUser: function(userinfo) {
+    return axios.post("api/home", userinfo);
   },
   // createUser:function(userinfo){
   //   return axios.post("/api/dashboard",userinfo)
   // },
 
-
-  getUserProfile: function(){
-  return axios.post('/api/dashboard')
-
-},///retrieves selected technologies
-  getUserPreference:function(e){
-    console.log("API.js file:***** expecting: jones.nadia.l@gmail.com ...Actual:")
-    console.log(e)
-  return axios.post('/api/dashboard',e)
-},
-
-  userPreference: function (e) {
-      console.log("API.js file:***** expecting: jones.nadia.l@gmail.com ...Actual:")
-      console.log(e)
-      return axios.get('/api/userPref', e)
+  getUserProfile: function() {
+    return axios.post("/api/dashboard");
+  }, ///retrieves selected technologies
+  getUserPreference: function(e) {
+    console.log(
+      "API.js file:***** expecting: jones.nadia.l@gmail.com ...Actual:"
+    );
+    console.log(e);
+    return axios.post("/api/dashboard", e);
   },
 
+  userPreference: function(e) {
+    console.log(
+      "API.js file:***** expecting: jones.nadia.l@gmail.com ...Actual:"
+    );
+    console.log(e);
+    return axios.get("/api/userPref", e);
+  },
 
+  updateUserProfile: function(updateInfo) {
+    console.log("API.js" + JSON.stringify(updateInfo));
+    return axios.put("/api/dashboard", updateInfo);
+  },
 
+  saveResources: function() {
+    return axios.put("/api/dashboard");
+  },
 
-  updateUserProfile:function(updateInfo){
-  console.log("API.js" + JSON.stringify(updateInfo))
-    return axios.put('/api/dashboard', updateInfo)
-},
+  deleteresource: function() {
+    return axios.put("/api/developer/");
+  },
 
-    saveResources: function() {
-        return axios.put('/api/dashboard')
-    },
+  showResourceList: function() {
+    return axios.get("/api/resources");
+  },
 
+  //these methods are used by the Connect Page
 
-    deleteresource: function() {
-        return axios.put('/api/developer/')
-    },
+  showConnection: function() {
+    console.log("this is showConnection");
+    return axios.get("/api/connect");
+  },
 
-    showResourceList: function() {
-        return axios.post('/api/resources')
-
-    },
-
-    //these methods are used by the Connect Page
-
-    showConnection: function() {
-        console.log("this is showConnection");
-        return axios.get('/api/connect')
-    },
-
-    saveUserConnection: function(userIndex, mentorIndex) {
-        console.log("this is saveUserConnection");
-        return axios.put('/api/developer/', {userIndex, mentorIndex})
-    }
-
+  saveUserConnection: function(userIndex, mentorIndex) {
+    console.log("this is saveUserConnection");
+    return axios.put("/api/developer/", { userIndex, mentorIndex });
+  }
 };
